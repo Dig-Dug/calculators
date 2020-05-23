175483d61551879858515dfb2fa851464ef4dd5a
Dig-Dug <40063087+Dig-Dug@users.noreply.github.com>
2020-05-23 14:26:28 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,52 @@
+package de.cbw.jav.calc;
+
+/**
+ * Simple calculator application class.
+ * @author Sascha Baumeister
+ */
+public class Calculator {
+
+	/**
+	 * Application entry point
+	 * @param args the runtime arguments (left operand, operator, right operand)
+	 * @throws IllegalArgumentException if the given operator symbol is illegal
+	 */
+	static public void main (final String[] args) {
+		final double leftOperand = Double.parseDouble(args[0]);
+		final double rightOperand = Double.parseDouble(args[2]);
+		final String operator = args[1];
+
+		final double result = calc (operator, leftOperand, rightOperand);
+		System.out.println(leftOperand + " " + operator + " " + rightOperand + " = " + result);
+	}
+
+
+	/**
+	 * Calculates the operator result.
+	 * @param leftOperand the left operand
+	 * @param operator the operator symbol (+, -, x, /, %, ^, #)
+	 * @param rightOperand the right operand)
+	 * @return the computation result
+	 * @throws IllegalArgumentException if the given operator symbol is illegal
+	 */
+	static private double calc (final String operator, final double leftOperand, final double rightOperand) {
+		switch (operator) {
+			case "+":
+				return leftOperand + rightOperand;
+			case "-":
+				return leftOperand - rightOperand;
+			case "x":
+				return leftOperand * rightOperand;
+			case "/":
+				return leftOperand / rightOperand;
+			case "%":
+				return leftOperand % rightOperand;
+			case "^":
+				return Math.pow(leftOperand, rightOperand);
+			case "#":
+				return Math.log(leftOperand) / Math.log(rightOperand);
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+}
